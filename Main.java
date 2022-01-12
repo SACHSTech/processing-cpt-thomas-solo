5d0e0fce0fdac1171729d07be4340c199346eb39
Eric Fabroa <eric.fabroa@ycdsbk12.ca>
2022-01-12 14:26:36 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -9,7 +9,10 @@ class Main {
   public static void main(String[] args) {
     
     String[] processingArgs = {"MySketch"};
-	  Sketch mySketch = new Sketch();
+	  Sketch mySketch = new Sketch();  //comment this out to run the other sketch files
+	  // Sketch1 mySketch = new Sketch1();  // uncomment this to run this sketch files
+	  // Sketch2 mySketch = new Sketch2();  // uncomment this to run this sketch files
+	  
 	  PApplet.runSketch(processingArgs, mySketch);
   }
   
