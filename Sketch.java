10853a41001ca60f0b826903882a6fd466997261
Thomas <thomas.martins26@ycdsbk12.ca>
2024-05-24 14:21:34 -0400

    Loaded

--- Sketch.java
+++ Sketch.java
@@ -1,36 +1,22 @@
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Sketch extends PApplet {
 	
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
+  PImage imgPlayer;
+
   public void settings() {
-	// put your size call here
-    size(400, 400);
+    size(960, 540);
   }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
   public void setup() {
-    background(210, 255, 173);
+    background(102, 204, 255);
+    imgPlayer = loadImage("Player.png");
+    imgPlayer.resize(imgPlayer.width*2, imgPlayer.height*2);
   }
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
-
-    stroke(255);
-    line(50, 125, 70, 50);  
+	  image(imgPlayer, 3, 3);
   }
   
-  // define other methods down here.
 }
